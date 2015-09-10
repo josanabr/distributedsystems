43ebd40a2da711a081cac7420fbd69e8db35dc79
John Sanabria <john.sanabria@correounivalle.edu.co>
2015-09-09 23:04:58 -0500

    new intro program

---
+++ threads/thread_101_1.c
@@ -0,0 +1,31 @@
+#include <pthread.h>
+#include <stdio.h>
+
+void *inc_x(void *x_void_ptr);
+
+int main() {
+	int x = 0,
+	    y = 0;
+	printf("x: %d, y: %d\n", x, y);
+	pthread_t inc_x_thread;
+	if (pthread_create(&inc_x_thread, NULL, inc_x, &x)) {
+		fprintf(stderr, "Error creating thread\n");
+		return 2;
+	}
+	while (++y < 100);
+	printf("y increment finished\n");
+	if (pthread_join(inc_x_thread, NULL)) {
+		fprintf(stderr, "Error joining thread\n");
+		return 2;
+	}
+	printf("x: %d, y: %d\n", x, y);
+
+	return 0;
+}
+
+void *inc_x(void *x_void_ptr) {
+	int *x_ptr = (int *)x_void_ptr;
+	while (++(*x_ptr) < 100);
+	printf("x increment finished\n");
+	return NULL;
+}
