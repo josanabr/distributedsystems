38e6fe9f2158e512b1535042ee8ceb45d65d2003
John Sanabria <johnsanabria@correounivalle.edu.co>
2020-03-17 10:24:36 -0500

    adding a pthread_exit() eample

---
+++ threads/thread_exit.c
@@ -0,0 +1,45 @@
+/******************************************************************************
+* FILE: hello.c
+* DESCRIPTION:
+*   A "hello world" Pthreads program.  Demonstrates thread creation and
+*   termination.
+* AUTHOR: Blaise Barney
+* LAST REVISED: 08/09/11
+******************************************************************************/
+#include <pthread.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#define NUM_THREADS	5
+
+void *PrintHello(void *threadid)
+{
+   long tid;
+   tid = (long)threadid;
+#ifdef __SLEEP__
+   sleep(2);
+#endif
+   printf("Hello World! It's me, thread #%ld!\n", tid);
+   pthread_exit(NULL);
+}
+
+int main(int argc, char *argv[])
+{
+   pthread_t threads[NUM_THREADS];
+   int rc;
+   long t;
+   for(t=0;t<NUM_THREADS;t++){
+     printf("In main: creating thread %ld\n", t);
+     rc = pthread_create(&threads[t], NULL, PrintHello, (void *)t);
+     if (rc){
+       printf("ERROR; return code from pthread_create() is %d\n", rc);
+       exit(-1);
+       }
+     }
+
+   /* Last thing that main() should do */
+#ifndef __NO_MAIN_EXIT__
+   pthread_exit(NULL);
+#endif
+}
+
